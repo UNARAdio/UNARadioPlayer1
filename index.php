404b5817764e5ba418875d18c2a7da506ab63203
UNARAdio <unaradiord@gmail.com>
2024-08-08 17:36:38 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,77 @@
+<head>
+    <meta charset="UTF-8">
+    <title>Player UNA Radio</title>
+    <link rel="shortcut icon" type="image/x-icon" href="https://unaradiord.000webhostapp.com/PLAYER2023/FAVICON.png"/>
+    
+</head>
+<body style="margin:0px">
+    <div id="lunaradio" style='width:100%; height:80px;
+-webkit-border-top-left-radius: 50px;
+  -webkit-border-top-right-radius: 50px;
+  -webkit-border-bottom-right-radius: 50px;
+  -webkit-border-bottom-left-radius: 50px;
+  -moz-border-radius-topleft: 50px;
+  -moz-border-radius-topright: 50px;
+  -moz-border-radius-bottomright: 50px;
+  -moz-border-radius-bottomleft: 50px;
+  border-top-left-radius: 50px;
+  border-top-right-radius: 50px;
+  border-bottom-right-radius: 50px;
+  border-bottom-left-radius: 50px;
+  margin-top: 8px!important;
+  border: none;'>
+        <div a href="PLAYER VISUAL" title="ZENO PRO">
+</a><a href="FRANCO" title="CREATIVO AGENCIA"></a>
+</div><script src="js/jquery-3.2.1.min.js" type="3bc5e4e5245e387f3290c38f-text/javascript"></script>
+<script src="js/lunaradio.min.js" type="3bc5e4e5245e387f3290c38f-text/javascript"></script>
+<div id="lunaradio" style="width:100%; height:550px;"></div>
+<script type="3bc5e4e5245e387f3290c38f-text/javascript">
+    
+$("#lunaradio").lunaradio({
+	
+	userinterface: "small",
+	backgroundcolor: "#f6a134",
+	fontcolor: "#ffffff",
+	hightlightcolor: "#2a3060",
+	fontname: "Saira Condensed",
+	googlefont: "Saira+Condensed:wght@100",
+	fontratio: "0.6",
+	radioname: "UNA RADIO",
+	scroll: "true",
+	coverimage: "https://unaradiord.000webhostapp.com/PLAYER2023/nocover.jpg",
+	usevisualizer: "fake",
+	coverstyle: "animated",
+	visualizertype: "4",
+	multicolorvisualizer: "true",
+    color1: "f6f6f6",
+    color2: "323edd",
+    color3: "fc5404",
+    color4: "#fa225b",
+	itunestoken: "1000lIPN",
+	metadatatechnic: "creativo-agencia-metadato",
+	ownmetadataurl: "https://zenoplay.zenomedia.com/api/zenofm/nowplaying/nasz1pwkga0uv",
+	streamurl: "https://stream.zeno.fm/nasz1pwkga0uv",
+	streamtype: "radiozeno",
+	icecastmountpoint: "/radio.mp3",
+	radionomyid: "",
+	radionomyapikey: "",
+	radiozenoid: "nasz1pwkga0uv",
+	radiojarid: "",
+	radiocoid: "",
+	shoutcastpath: "/stream",
+	shoutcastid: "1",
+	streamsuffix: "",
+	metadatainterval: "20000",
+	volume: "85",
+	debug: "false",
+	usestreamcorsproxy: "false", 
+	corsproxy: "https://corsradio.herokuapp.com/index.php?q=",
+	autoplay:"true",
+});
+
+</script>
+<script src="https://ajax.cloudflare.com/cdn-cgi/scripts/7089c43e/cloudflare-static/rocket-loader.min.js" data-cf-settings="3bc5e4e5245e387f3290c38f-|49" defer=""></script>
+
+
+</script>
+</body>
